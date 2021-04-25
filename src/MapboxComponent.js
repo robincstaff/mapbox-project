aec9af0ca2b56036d8aab886158608622a3b2444
Robin <robincs@stud.ntnu.no>
2021-04-25 22:26:08 +0200

    can now import and display google location data

--- src/MapboxComponent.js
+++ src/MapboxComponent.js
@@ -4,6 +4,7 @@ import mapboxgl from 'mapbox-gl/dist/mapbox-gl-csp';
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import MapboxWorker from 'worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker';
 import mapboxGlCsp from 'mapbox-gl/dist/mapbox-gl-csp';
+import data from './LocationHistory/SemanticLocationHistory/2021/2021_MARCH.json';
 
 mapboxgl.workerClass = MapboxWorker;
 mapboxgl.accessToken = 'pk.eyJ1Ijoicm9iaW5jcyIsImEiOiJja25tNWdtbGcwb3p5MnNuMHlqaTN0emQxIn0.NnfO1gt2sCMYDF_Mag8q6g';
@@ -17,8 +18,7 @@ class Mapbox extends React.PureComponent {
       zoom: 11
     }
     this.mapContainer = React.createRef();
-    const data = require("C:\\Users\\robin\\Documents\\Robin Google Location History\\Semantic Location History\\2021\\2021_MARCH.json");
-    this.locationData = JSON.parse(data);
+    this.locationData = data;
   }
 
   componentDidMount() {
@@ -37,7 +37,27 @@ class Mapbox extends React.PureComponent {
         zoom: map.getZoom().toFixed(2)
       });
     });
-    map.addSource(this.locationData);
+    let marker = new mapboxgl.Marker()
+    .setLngLat([10.40316447660307, 63.44198412217757])
+    .addTo(map);
+    this.addMarkers(map);
+  }
+
+  addMarkers(map) {
+      this.locationData.timelineObjects.forEach((location) => {
+        if(location.placeVisit) {
+          new mapboxgl.Marker()
+          .setLngLat([this.convertCoordinates(location.placeVisit.location.longitudeE7), this.convertCoordinates(location.placeVisit.location.latitudeE7)])
+          .addTo(map)
+        }
+        //Handle activity segments for future work
+      });
+  }
+
+  convertCoordinates(coordinate) {
+    let stringCor = String(coordinate);
+    let converted = parseFloat(stringCor.substring(0, 2).concat(".", stringCor.substring(2)));
+    return converted;
   }
 
   render() {
